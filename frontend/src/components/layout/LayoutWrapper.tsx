91746f6cb5b02a664eb37fc0824fc3d7e49ac06f
Muhammad-Danish-011 <softwareengineer.danish@gmail.com>
2025-08-15 07:28:11 -0700

    updated

--- frontend/src/components/layout/LayoutWrapper.tsx
+++ frontend/src/components/layout/LayoutWrapper.tsx
@@ -22,7 +22,7 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
         <main
           className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8"
           style={{
-            backgroundImage: "url('/erp.jpg')",
+            backgroundImage: "url('/erp.png')",
             backgroundSize: "100% 100%",
             backgroundRepeat: "no-repeat",
             backgroundAttachment: "fixed",
